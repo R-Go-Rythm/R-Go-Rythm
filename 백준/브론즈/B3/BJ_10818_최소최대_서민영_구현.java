40f4bf384d4313e2ed33a97a8685dfdbb3ebc765
smy999 <33407191+smy999@users.noreply.github.com>
2022-01-05 21:38:26 +0900

    Create BJ_10818_최소최대_서민영_구현.java

---
+++ 백준/브론즈/B3/BJ_10818_최소최대_서민영_구현.java
@@ -0,0 +1,27 @@
+package Jan_week2;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_10818_최소최대_구현 {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int min = Integer.MAX_VALUE;
+		int max = Integer.MIN_VALUE;
+		
+		// 입력된 수에 대해 모두 최소 최대 검사를 진행한다.
+		for(int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			min = Math.min(min,  num);
+			max = Math.max(max,  num);
+		}
+		
+		System.out.print(min + " " + max);
+	}
+}
