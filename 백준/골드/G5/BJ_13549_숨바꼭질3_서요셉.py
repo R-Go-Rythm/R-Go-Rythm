68ea640eb2fffaa46930f0e4f3e1e08bf1cd4755
yoseph0310 <56868159+yoseph0310@users.noreply.github.com>
2022-02-13 17:34:01 +0900

    :sparkles: BJ 13549 숨바꼭질3

---
+++ 백준/골드/G5/BJ_13549_숨바꼭질3_서요셉.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+n, k = map(int, input().split())
+visited = [-1 for _ in range(100001)]
+
+def bfs(n):
+    q = deque()
+    q.append(n)
+    visited[n] = 0
+
+    while q:
+        x = q.popleft()
+
+        if x == k:
+            print(visited[x])
+            break
+        if 0 <= x-1 < 100001 and visited[x-1] == -1:
+            visited[x-1] = visited[x] + 1
+            q.append(x-1)
+        if 0 < x*2 < 100001 and visited[x*2] == -1:
+            visited[x*2] = visited[x]
+            q.appendleft(x*2)
+        if 0 <= x+1 < 100001 and visited[x+1] == -1:
+            visited[x+1] = visited[x] + 1
+            q.append(x+1)
+
+bfs(n)
